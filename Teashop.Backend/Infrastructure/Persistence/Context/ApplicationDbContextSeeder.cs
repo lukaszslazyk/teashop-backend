631471a91f10ca0f18f505231961871ac85ba7fc
Łukasz Ślazyk <lukasz.slazyk@gmail.com>
2020-11-08 18:39:38 +0100

    Added Category and ProductCategory entities

--- Teashop.Backend/Infrastructure/Persistence/Context/ApplicationDbContextSeeder.cs
+++ Teashop.Backend/Infrastructure/Persistence/Context/ApplicationDbContextSeeder.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using Teashop.Backend.Domain.Product.Entities;
 
@@ -15,8 +14,49 @@ namespace Teashop.Backend.Infrastructure.Persistence.Context
 
         private static void SeedProducts(ApplicationDbContext context)
         {
-            if (context.Products.Any())
+            if (context.Products.Any() || context.Categories.Any())
                 return;
+
+            var tea = new Category
+            {
+                Name = "Tea"
+            };
+            var greenTea = new Category
+            {
+                Name = "GreenTea",
+                ParentCategory = tea
+            };
+            var blackTea = new Category
+            {
+                Name = "BlackTea",
+                ParentCategory = tea
+            };
+            var redTea = new Category
+            {
+                Name = "RedTea",
+                ParentCategory = tea
+            };
+            var whiteTea = new Category
+            {
+                Name = "WhiteTea",
+                ParentCategory = tea
+            };
+            var herbs = new Category
+            {
+                Name = "Herbs",
+            };
+            var accessories = new Category
+            {
+                Name = "Accessories",
+            };
+
+            context.Categories.Add(tea);
+            context.Categories.Add(greenTea);
+            context.Categories.Add(blackTea);
+            context.Categories.Add(redTea);
+            context.Categories.Add(whiteTea);
+            context.Categories.Add(herbs);
+            context.Categories.Add(accessories);
 
             var products = new List<ProductEntity>
             {
@@ -24,19 +64,66 @@ namespace Teashop.Backend.Infrastructure.Persistence.Context
                 {
                     Name = "Sencha",
                     Price = 19.99,
-                    QuantityPerPrice = 100
+                    QuantityPerPrice = 100,
+                    Categories =
+                    {
+                        new ProductCategory
+                        {
+                            Category = greenTea
+                        }
+                    }
                 },
                 new ProductEntity
                 {
                     Name = "Bancha",
                     Price = 9.99,
-                    QuantityPerPrice = 100
+                    QuantityPerPrice = 100,
+                    Categories =
+                    {
+                        new ProductCategory
+                        {
+                            Category = greenTea
+                        }
+                    }
                 },
                 new ProductEntity
                 {
                     Name = "Gyokuro",
                     Price = 59.99,
-                    QuantityPerPrice = 100
+                    QuantityPerPrice = 100,
+                    Categories =
+                    {
+                        new ProductCategory
+                        {
+                            Category = greenTea
+                        }
+                    }
+                },
+                new ProductEntity
+                {
+                    Name = "Darjeeling",
+                    Price = 19.99,
+                    QuantityPerPrice = 100,
+                    Categories =
+                    {
+                        new ProductCategory
+                        {
+                            Category = blackTea
+                        }
+                    }
+                },
+                new ProductEntity
+                {
+                    Name = "Pu-erh",
+                    Price = 79.99,
+                    QuantityPerPrice = 100,
+                    Categories =
+                    {
+                        new ProductCategory
+                        {
+                            Category = redTea
+                        }
+                    }
                 },
             };
 
---
+++ Teashop.Backend/Infrastructure/Persistence/Migrations/20201108172617_AddCategoriesAndProductCategories.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Teashop.Backend.Migrations
+{
+    public partial class AddCategoriesAndProductCategories : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    CategoryId = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    ParentCategoryCategoryId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CategoryId);
+                    table.UniqueConstraint("AK_Categories_Name", x => x.Name);
+                    table.ForeignKey(
+                        name: "FK_Categories_Categories_ParentCategoryCategoryId",
+                        column: x => x.ParentCategoryCategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "CategoryId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductCategories",
+                columns: table => new
+                {
+                    ProductId = table.Column<Guid>(nullable: false),
+                    CategoryId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductCategories", x => new { x.ProductId, x.CategoryId });
+                    table.ForeignKey(
+                        name: "FK_ProductCategories_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "CategoryId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProductCategories_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "ProductId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_ParentCategoryCategoryId",
+                table: "Categories",
+                column: "ParentCategoryCategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductCategories_CategoryId",
+                table: "ProductCategories",
+                column: "CategoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductCategories");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
